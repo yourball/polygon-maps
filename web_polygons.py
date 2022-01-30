cd2ec1817100a4e8f08c3f7654dd3c4d2815eef2
Yaroslav Kharkov <y.kharkov@gmail.com>
2022-01-29 23:31:32 -0500

    More text

--- web_polygons.py
+++ web_polygons.py
@@ -44,12 +44,16 @@ def orbit(
 def plot_orbits(ax, k_list, xi_list, d, Tmax=1000):
     x0_list = np.linspace(0, 10, 50)
     max_x0 = max(x0_list)
-    for x0 in x0_list:
+    cm = plt.get_cmap('gist_rainbow')
+    NUM_COLORS = len(x0_list)
+    for i, x0 in enumerate(x0_list):
         y0 = x0*1.01
         traj_data = orbit(x0, y0, k_list, xi_list, d, Tmax=Tmax)
 
-        ax.scatter(traj_data[:, 0], traj_data[:, 1], s=1,
-                   color=[abs(x0)/max_x0, .1*abs(x0)/max_x0, 1-abs(x0)/max_x0])
+        ax.scatter(traj_data[:, 0], traj_data[:, 1], s=0.25,
+                   color=cm(1.*i/NUM_COLORS)
+                   #color=[abs(x0)/max_x0, .5*abs(x0)/max_x0, 1-abs(x0)/max_x0]
+                   )
     return ax
 
 st.title('Integrable symplectic mappings of the plane')
@@ -67,36 +71,48 @@ $$""")
 
 k_list = []
 
-num_pieces = st.number_input('Enter a number of piecewise regions',
+st.write("""
+
+
+        """)
+
+col1, col2 = st.columns(2)
+
+num_pieces = col1.number_input('Number of piecewise regions for the force function f(q)',
                              min_value=2, max_value=10, value=3, step=1)
-st.text(r'Slopes of the piesewise function')
+Tmax = col2.number_input("Number of map iterations", min_value=1, value=2000, step=1000)
+
+st.write(r'Slopes of the piesewise function $k_i$:')
 for p in range(int(num_pieces)):
     k_list.append(st.slider(f'k{p}', min_value=-3, max_value=3, value=0))
 
 d = st.slider('Shift parameter, d', min_value=-10, max_value=10, value=0)
 xi_list = np.arange(len(k_list)-1)
 
-st.subheader('Mapping phase portrait')
+
+st.subheader('Phase portrait of the map')
 
 fig_map, ax_map = plt.subplots()
-ax_map.set_xlabel('q')
-ax_map.set_ylabel('p')
-ax_map = plot_orbits(ax_map, k_list, xi_list, d=d, Tmax=1000)
+ax_map.set_xlabel(r'$q$')
+ax_map.set_ylabel(r'$p$')
+ax_map = plot_orbits(ax_map, k_list, xi_list, d=d, Tmax=int(Tmax))
+ax_map.set_aspect('equal')
 st.pyplot(fig_map)
 
 st.subheader('Force function')
 # plot force function
 fig_f, ax_f = plt.subplots()
 x = np.linspace(min(xi_list)-2, max(xi_list)+2, 1000)
-ax_f.set_xlabel('q')
-ax_f.set_ylabel('f(q)')
+ax_f.set_xlabel(r'$q$')
+ax_f.set_ylabel(r'$f(q)$')
 f = []
 for xi in x:
     f.append(force(xi, k_list, xi_list, d))
-ax_f.plot(x, f)
+
+ax_f.plot(x, f, color='k')
 
 st.pyplot(fig_f)
 st.write("""Contributors:
-        Yaroslav Kharkov,
-        Timothy Zolkin
+        Yaroslav Kharkov (Univeristy of Maryland),
+        Timothy Zolkin (Fermilab)
         """)
